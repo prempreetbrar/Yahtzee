d7a26c6fcdf9ce1e5625c2ce38f46904c2debb57
prempreetbrar <prempreet.brar@ucalgary.ca>
2022-08-08 23:48:36 -0600

    [yaht-5] Refactor of ScoreCalculator

--- src/ScoreCalculator.js
+++ src/ScoreCalculator.js
@@ -1,43 +1,51 @@
-function scoreUpperSection(number, dice) {
-  return count(dice, number) * number;
+function scoreUpperSection(die, dice) {
+  return count(dice, die) * die;
 }
 
 function threeOfAKind(dice) {
-  console.log("threeOfAKind is invoked")
-  if (dice.some(number => count(dice, number) >= 3))
-    return dice.reduce((total, number) => total + number);
+  /* if there exists some die that occurs at least three times, then we add
+     all the dice */
+  if (dice.some(die => count(dice, die) >= 3))
+    return dice.reduce((total, die) => total + die);
   return 0;
 }
 
-function count(dice, number) {
-  return dice.filter(n => n === number).length;
+function count(dice, die) {
+  return dice.filter(d => d === die).length;
 }
 
 function fourOfAKind(dice) {
-  if (dice.some(number => count(dice, number) >= 4))
-    return dice.reduce((total, number) => total + number);
+  /* if there exists some die that occurs at least four times, then we add
+     all the dice */
+  if (dice.some(die => count(dice, die) >= 4))
+    return dice.reduce((total, die) => total + die);
   return 0;
 }
 
 function fullHouse(dice) {
-  if (dice.some(number => count(dice, number) === 3) && 
-      dice.some(number => count(dice, number) === 2)
-  )
-    return 25;
+  if (dice.some(die => count(dice, die) === 3) && 
+      dice.some(die => count(dice, die) === 2))
+        return 25;
   return 0;
 }
 
 function smallStraight(dice) {
+  /* for a small straight, if the dice include 2,3,4, then in the remaining
+     dice there must be a 1 or a 5 */
   if ((dice.includes(2) && dice.includes(3) && dice.includes(4)) &&
-         (dice.includes(1) || dice.includes(5))
-         ||
-         ((dice.includes(3) && dice.includes(4) && dice.includes(5)) &&
-         (dice.includes(2) || dice.includes(6))))
-         return 30;
+      (dice.includes(1) || dice.includes(5)))
+        return 30;
+  /* for a small straight, if the dice include 3,4,5, then in the remaining
+     dice there must be a 2 or a 6 */
+  else if ((dice.includes(3) && dice.includes(4) && dice.includes(5)) &&
+           (dice.includes(2) || dice.includes(6)))
+              return 30;
   return 0;
 }
 
 function largeStraight(dice) {
+  /* a large straight must have 2,3,4,5, and the remaining dice should either be
+     a 1 or a 6 */
   if ((dice.includes(2) && dice.includes(3) && dice.includes(4) && dice.includes(5))
         && (dice.includes(1) || dice.includes(6)))
       return 40;
@@ -45,12 +53,23 @@ function largeStraight(dice) {
 }
 
 function yahtzee(dice) {
+  /* if there is only one unique dice, then that means all are the same
+     and we have a yahtzee */
   if ((new Set(dice)).size === 1) return 50;
   return 0;
 }
 
 function chance(dice) {
-  return dice.reduce((acc, currDie) => acc + currDie);
+  return dice.reduce((total, die) => total + die);
 }
 
-export {scoreUpperSection, threeOfAKind, fourOfAKind, fullHouse, chance, smallStraight, largeStraight, yahtzee};
+export {
+  scoreUpperSection, 
+  threeOfAKind, 
+  fourOfAKind, 
+  fullHouse, 
+  chance, 
+  smallStraight, 
+  largeStraight, 
+  yahtzee
+};
